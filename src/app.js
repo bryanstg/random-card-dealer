0b6a943dbfae37e352a0c5f8496a5b6888d5fd93
Bryan García <bryan1997st@gmail.com>
2021-04-10 00:56:14 -0500

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♣", "♠", "♥", "♦"];
+  const cardValues = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  function randomCard() {
+    let randomNumberSuit = Math.floor(Math.random() * 4);
+    let randomValue = Math.floor(Math.random() * 13);
+
+    return {
+      suit: suits[randomNumberSuit],
+      value: cardValues[randomValue]
+    };
+  }
+
+  let cardContent = randomCard();
+
+  const table = document.getElementById("table");
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `
+    <div class="card-data" id="top">${cardContent.suit}</div>
+    <div class="card-data" id="middle">${cardContent.value}</div>
+    <div class="card-data" id="bottom">${cardContent.suit}</div>
+  `;
+  table.appendChild(card);
 };
