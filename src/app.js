dc7be377bd1b2b3fbd0f73b47f48edd10b9e7dfd
Bryan García <bryan1997st@gmail.com>
2021-04-10 20:45:07 -0500

    Modificación en las variables y los nombres

--- src/app.js
+++ src/app.js
@@ -6,12 +6,12 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   const suits = ["♣", "♠", "♥", "♦"];
   const cardValues = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
   function randomCard() {
-    let randomNumberSuit = Math.floor(Math.random() * 4);
-    let randomValue = Math.floor(Math.random() * 13);
+    const randomNumberSuit = Math.floor(Math.random() * 4);
+    const randomValue = Math.floor(Math.random() * 13);
 
     return {
       suit: suits[randomNumberSuit],
